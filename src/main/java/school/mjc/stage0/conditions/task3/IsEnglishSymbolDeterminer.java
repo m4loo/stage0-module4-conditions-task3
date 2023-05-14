a8f4e95cae4cc5b5cf0971bc98c5f153faf74357
m4loo <128167781+m4loo@users.noreply.github.com>
2023-05-15 03:55:39 +0700

    done

--- src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
@@ -2,5 +2,8 @@ package school.mjc.stage0.conditions.task3;
 
 public class IsEnglishSymbolDeterminer {
     public void isEnglishSymbol(char symbol) {
+        if ((symbol >= 97 && symbol <= 112) || (symbol >= 65 && symbol <= 90)) {
+            System.out.println("English");
+        } else System.out.println("Non English");
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/Seasons.java
+++ src/main/java/school/mjc/stage0/conditions/task3/Seasons.java
@@ -2,6 +2,14 @@ package school.mjc.stage0.conditions.task3;
 
 public class Seasons {
     public void tellTheSeasonByMonthNumber(int month) {
-
+        if ((month >= 1 && month <= 2) || month == 12) {
+            System.out.println("Winter");
+        } else if (month >= 3 && month < 6) {
+            System.out.println("Spring");
+        } else if (month >= 6 && month < 9) {
+            System.out.println("Summer");
+        } else if (month >= 9 && month < 12) {
+            System.out.println("Autumn");
+        } else System.out.println("wrong number!");
     }
 }
